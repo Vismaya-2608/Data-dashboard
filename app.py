0150aa5904e9c122f1a6137740f4ba35a6a7bad5
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-06-13 17:02:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import os
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+
+st.set_page_config(layout="wide")
+st.markdown("Data Summary of New Files")
+
+
